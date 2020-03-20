bf74448d9f49f13cab1383a50259f107817ae5b3
malvger <malvger@gmail.com>
2020-03-20 08:54:42 -0600

    Modificacion de proyecto angular

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -3,10 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './component/toolbar/toolbar.component';
+import { LetSidebarComponent } from './component/let-sidebar/let-sidebar.component';
+import { PageWrapperComponent } from './component/page-wrapper/page-wrapper.component';
+import { NavbarComponent } from './component/contener/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    LetSidebarComponent,
+    PageWrapperComponent,
+    NavbarComponent
+
   ],
   imports: [
     BrowserModule,
